cc15961c9b3215230f81a5acb4666a83068e406c
gsh4124-cyber <gsh4124@gmail.com>
2026-09-03 16:32:37 +0900

    style: add spacing below copied scripture reference

--- clipboard.js
+++ clipboard.js
@@ -62,7 +62,7 @@
   }
 
   function buildCopyText(startVerse, endVerse, lines) {
-    return `[${translationName()}] ${refParts(startVerse, endVerse)}\n${lines.join('\n')}\n\n${siteLabel()}\n${SITE_URL}`;
+    return `[${translationName()}] ${refParts(startVerse, endVerse)}\n\n${lines.join('\n')}\n\n${siteLabel()}\n${SITE_URL}`;
   }
 
   versesRoot.addEventListener('copy', (event) => {
